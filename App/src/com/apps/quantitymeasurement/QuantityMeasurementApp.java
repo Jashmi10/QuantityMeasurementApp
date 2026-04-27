b566630f55f2dc121fd639f3d2a13eb65a523f6d
Jashmi10 <jashmirajij@gmail.com>
2026-04-27 15:20:22 +0530

    UC2 done

--- App/src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ App/src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,6 +2,7 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
+    // 🔹 Inner class for Feet
     public static class Feet {
         private final double value;
 
@@ -24,10 +25,48 @@ public class QuantityMeasurementApp {
         }
     }
 
-    public static void main(String[] args) {
+    // 🔹 Inner class for Inches (NEW in UC2)
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null) return false;
+
+            if (getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // 🔹 Static method for Feet equality
+    public static void demonstrateFeetEquality() {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
 
-        System.out.println(f1.equals(f2));
+        System.out.println("Feet equality: " + f1.equals(f2));
+    }
+
+    // 🔹 Static method for Inches equality
+    public static void demonstrateInchesEquality() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        System.out.println("Inches equality: " + i1.equals(i2));
+    }
+
+    // 🔹 Main method
+    public static void main(String[] args) {
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
     }
 }
